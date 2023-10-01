fd7f647832e80ef2ef8241efb9eb7dea6057fbcd
Maxim <MaximSw@web.de>
2023-10-01 13:36:23 +0200

    drones fixed

--- Source/TiberiumRim/Comps/Comp_DroneStation.cs
+++ Source/TiberiumRim/Comps/Comp_DroneStation.cs
@@ -16,12 +16,8 @@ public class Comp_DroneStation : Comp_MechStation
         base.PostSpawnSetup(respawningAfterLoad);
         radialCells = GenRadial.NumCellsInRadius(Props.radius);
         if (MainMechLink.LinkedMechs.NullOrEmpty())
-        {
-            for (int i = 0; i < Props.garageCapacity; i++)
-            {
+            for (var i = 0; i < Props.garageCapacity; i++)
                 TryAddMech(MakeMech(Props.mechKindDef), true);
-            }
-        }
     }
 
     public override void PostExposeData()
@@ -29,7 +25,7 @@ public class Comp_DroneStation : Comp_MechStation
         base.PostExposeData();
     }
 
-    public new CompProperties_DroneStation Props => base.props as CompProperties_DroneStation;
+    public new CompProperties_DroneStation Props => props as CompProperties_DroneStation;
     public ThingOwner DroneGarage => MainGarage.Container;
     public CompRefuelable FuelComp => parent.GetComp<CompRefuelable>();
 
@@ -38,27 +34,24 @@ public class Comp_DroneStation : Comp_MechStation
     public override void CompTick()
     {
         base.CompTick();
-        if (IsPowered)
-        {
-            TryReleaseDrone();
-        }
+        if (IsPowered) TryReleaseDrone();
     }
 
     public void TryReleaseDrone()
     {
         if (!FuelComp.HasFuel) return;
-        if(MainMechLink.Count == 0) return;
+        if (MainMechLink.Count == 0) return;
         if (!AnyMechAvailableForRepair) return;
         foreach (var brokeyMech in MechsAvailableForRepair)
-        {
             for (var i = MainMechLink.Count - 1; i >= 0; i--)
             {
                 var drone = (RepairDrone)MainMechLink[i];
                 if (Reservations.IsReserved(brokeyMech)) continue;
-                
+
                 var closestPos = GenAdjFast.AdjacentCells8Way(parent).MinBy(c => c.DistanceTo(brokeyMech.Position));
                 if (drone.Spawned && Reservations.FirstReservationFor(drone) != null) continue;
-                if (!drone.Spawned && !MainGarage.TryPullFromGarage(drone, out Thing mech, closestPos, parent.Map, ThingPlaceMode.Direct)) continue;
+                if (!drone.Spawned &&
+                    !MainGarage.TryPullFromGarage(drone, out var mech, closestPos, parent.Map)) continue;
                 var job = new JobWithExtras(DefDatabase<JobDef>.GetNamed("RepairMechanicalPawn"), brokeyMech)
                 {
                     loadID = Find.UniqueIDsManager.GetNextJobID(),
@@ -67,23 +60,22 @@ public class Comp_DroneStation : Comp_MechStation
                 Reservations.Reserve(drone, job, brokeyMech);
                 drone.jobs.StartJob(job);
                 parent.Map.mapDrawer.MapMeshDirty(parent.Position, MapMeshFlag.Things);
-
             }
-        }
     }
 
     public bool AnyMechAvailableForRepair
     {
         get
         {
-            for (int i = 0; i < radialCells; i++)
+            for (var i = 0; i < radialCells; i++)
             {
-                var pos = this.parent.Position + GenRadial.RadialPattern[i];
+                var pos = parent.Position + GenRadial.RadialPattern[i];
                 if (!pos.InBounds(parent.Map)) continue;
                 var pawn = pos.GetFirstPawn(parent.Map);
                 if (pawn is not MechanicalPawn mech || !mech.IsDamaged()) continue;
                 return true;
             }
+
             return false;
         }
     }
@@ -92,9 +84,9 @@ public class Comp_DroneStation : Comp_MechStation
     {
         get
         {
-            for (int i = 0; i < radialCells; i++)
+            for (var i = 0; i < radialCells; i++)
             {
-                var pos = this.parent.Position + GenRadial.RadialPattern[i];
+                var pos = parent.Position + GenRadial.RadialPattern[i];
                 if (!pos.InBounds(parent.Map)) continue;
                 var pawn = pos.GetFirstPawn(parent.Map);
                 if (pawn is not MechanicalPawn mech || !mech.IsDamaged()) continue;
@@ -107,7 +99,7 @@ public class Comp_DroneStation : Comp_MechStation
     {
         get
         {
-            CompPowerTrader comp = this.parent.TryGetComp<CompPowerTrader>();
+            var comp = parent.TryGetComp<CompPowerTrader>();
             return comp?.PowerOn ?? true;
         }
     }
@@ -115,10 +107,7 @@ public class Comp_DroneStation : Comp_MechStation
     public override void PostDraw()
     {
         base.PostDraw();
-        if (Find.Selector.IsSelected(this.parent))
-        {
-            GenDraw.DrawRadiusRing(this.parent.TrueCenter().ToIntVec3(), Props.radius);
-        }
+        if (Find.Selector.IsSelected(parent)) GenDraw.DrawRadiusRing(parent.TrueCenter().ToIntVec3(), Props.radius);
     }
 
     public override void PostPrintOnto(SectionLayer layer)
@@ -136,13 +125,14 @@ public class Comp_DroneStation : Comp_MechStation
     private void PrintIdleDrones(SectionLayer layer)
     {
         if (DroneGarage.Count == 0) return;
-        for(int i = 0; i < DroneGarage.Count; i++)
+        for (var i = 0; i < DroneGarage.Count; i++)
         {
-            Vector3 drawPos = parent.DrawPos + new Vector3(0, AltitudeLayer.BuildingOnTop.AltitudeFor(), 0) + Props.renderOffsets[i];
-            RepairDrone drone = (DroneGarage as ThingOwner<MechanicalPawn>).innerList[0] as RepairDrone;
-            Graphic droneGraphic = drone.Drawer.renderer.graphics.nakedGraphic;
-            Material mat = droneGraphic.MatSouth;
-            Printer_Plane.PrintPlane(layer, drawPos, new Vector2(Props.renderSize, Props.renderSize), mat, 0, false);
+            var drawPos = parent.DrawPos + new Vector3(0, AltitudeLayer.BuildingOnTop.AltitudeFor(), 0) +
+                          Props.renderOffsets[i];
+            var drone = (DroneGarage as ThingOwner<MechanicalPawn>).innerList[0] as RepairDrone;
+            var droneGraphic = drone.Drawer.renderer.graphics.nakedGraphic;
+            var mat = droneGraphic.MatSouth;
+            Printer_Plane.PrintPlane(layer, drawPos, new Vector2(Props.renderSize, Props.renderSize), mat);
         }
     }
 
@@ -180,5 +170,17 @@ public class CompProperties_DroneStation : CompProperties_MechStation
     public CompProperties_DroneStation()
     {
         this.compClass = typeof(Comp_DroneStation);
+    }
+
+    public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+    {
+        foreach (var error in base.ConfigErrors(parentDef))
+        {
+            yield return error;
+        }
+        if (!parentDef.comps.Exists(c => c is CompProperties_Refuelable))
+        {
+            yield return "A drone station requires a refuelable comp!";
+        }
     }
 }
